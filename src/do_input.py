5d692e9209bd3f1fa2e4ff3deaff9c48a0ae332f
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-11-14 22:38:44 +0800

    hello

---
+++ src/do_input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Created on 2017年11月14日
+
+@author: taomk
+'''
+myName = input("请输入你的名字：")
+print("hello,", myName, ":)")
---
+++ src/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Created on 2017年11月14日
+
+@author: taomk
+'''
+print("hello", "world")
+print(unicode("中文", encoding="utf-8"))
+print("100 + 2.5 = ", 100+2.5)
